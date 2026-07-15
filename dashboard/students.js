91d260b8cb09b59043449e1d567d814624712fc2
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-15 21:50:02 +0530

    updated

--- dashboard/students.js
+++ dashboard/students.js
@@ -3,3 +3,28 @@ let students = JSON.parse(localStorage.getItem("students")) || [];
 function saveStudents() {
     localStorage.setItem("students", JSON.stringify(students));
 }
+function renderStudents() {
+
+    const tbody = document.getElementById("studentTableBody");
+
+    tbody.innerHTML = "";
+
+    students.forEach(student => {
+
+        tbody.innerHTML += `
+            <tr>
+                <td>${student.name}</td>
+                <td>${student.email}</td>
+                <td>${student.attendance}%</td>
+                <td>${student.performance}</td>
+                <td>${student.status}</td>
+                <td>
+                    <button>Edit</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        `;
+
+    });
+
+}
