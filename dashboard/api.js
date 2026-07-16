f9afbd8ae9565d0ebe64ac084abfe6443c54a60a
anushkayadav03 <anuska.yadav0303@gmail.com>
2026-07-16 14:48:52 +0530

    updated

---
+++ dashboard/api.js
@@ -0,0 +1,39 @@
+// API URLs
+const USERS_API = "https://dummyjson.com/users";
+const REPORTS_API = "https://jsonplaceholder.typicode.com/posts";
+
+// Fetch Users
+async function fetchUsers() {
+    try {
+        const response = await fetch(USERS_API);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch users.");
+        }
+
+        const data = await response.json();
+        return data.users;
+
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+// Fetch Reports
+async function fetchReports() {
+    try {
+        const response = await fetch(REPORTS_API);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch reports.");
+        }
+
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
